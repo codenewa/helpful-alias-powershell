a1cdb94f7760e58111e54a2441d2faf85a1ea10a
Ashish Shakya <ashish.shakya@gmail.com>
2022-03-17 13:56:08 -0500

    Create profile.ps1

---
+++ profile.ps1
@@ -0,0 +1,6 @@
+new-alias e explorer
+new-alias clean c:\tools\clean.ps1
+new-alias vs "c:\Program Files\Microsoft Visual Studio\2022\Professional\Common7\IDE\devenv.exe"
+new-alias vs19 "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\Common7\IDE\devenv.exe"
+new-alias find c:\tools\find.ps1
+new-alias admin c:\tools\start-as-admin.ps1
