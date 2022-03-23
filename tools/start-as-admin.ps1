9edf6583d050075db8a5166b5472d46763f9a977
Ashish Shakya <ashish.shakya@gmail.com>
2022-03-23 08:28:17 -0500

    use saved password

--- tools/start-as-admin.ps1
+++ tools/start-as-admin.ps1
@@ -1,9 +1,11 @@
 param([string] $command)
 
-$username="your-user-name"
-$password="your-password"
-
-$credentials = new-object system.management.automation.pscredential -argumentlist @($username,(convertto-securestring -string $password -asplaintext -force))
+$username="admin_as@texpac"
+$encPasswd = Get-content c:\tools\creds-a.bin
+$password = convertTo-secureString $encPasswd
+# use this for plaintext password
+#$password=  (convertto-securestring -string $plainTextPassword -asplaintext -force)
+$credentials = new-object system.management.automation.pscredential -argumentlist @($username,$password)
 # $credentials = get-credential
 
 $commandPath = ""
@@ -14,7 +16,7 @@ switch($command)
 	"np" { $commandPath = "C:\Program Files\Notepad++\notepad++.exe"; }
 	"code" {$commandPath = "code"} 
 	"wt"{ $commandPath = "C:\Program Files\WindowsApps\Microsoft.WindowsTerminal_1.11.3471.0_x64__8wekyb3d8bbwe\wt.exe" }
-	default { Write-Host "The path for this command: $command is not registered."}
+	default { Write-Host "This alias allows you to open np:notepad++, code: VSCode, wt: Windows Terminal with your admin credentials. If you haven't set your credentials yet, use the `passwd -admin` command to set your admin password." }
 }
 
 if($commandPath -ne ''){
