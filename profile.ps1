483a1cf4e702dc32587064237837e6deb703e5c5
Ashish Shakya <ashish.shakya@gmail.com>
2022-03-23 08:28:30 -0500

    Update profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -1,6 +1,14 @@
+#shortcuts 
 new-alias e explorer
-new-alias clean c:\tools\clean.ps1
 new-alias vs "c:\Program Files\Microsoft Visual Studio\2022\Professional\Common7\IDE\devenv.exe"
 new-alias vs19 "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\Common7\IDE\devenv.exe"
+
+# useful scripts
+new-alias clean c:\tools\clean.ps1
 new-alias find c:\tools\find.ps1
+
+# starts a process in admin mode.
 new-alias admin c:\tools\start-as-admin.ps1
+
+# this sets up the password to be used in the admin alias when starting a process in admin mode.
+new-alias passwd c:\tools\set-password.ps1
