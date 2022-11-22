a1c93334f4b10d21486b6263bc8591e942332ec8
Ashish Shakya <ashakya@tpg.com>
2022-11-22 10:46:00 -0600

    [#] updated profile

--- profile.ps1
+++ profile.ps1
@@ -1,14 +1,23 @@
 #shortcuts 
 new-alias e explorer
+new-alias c code
 new-alias vs "c:\Program Files\Microsoft Visual Studio\2022\Professional\Common7\IDE\devenv.exe"
 new-alias vs19 "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\Common7\IDE\devenv.exe"
-
+new-alias ssms "C:\Program Files (x86)\Microsoft SQL Server Management Studio 18\Common7\IDE\Ssms.exe"
+new-alias azds "C:\Users\ashakya\AppData\Local\Programs\Azure Data Studio\azuredatastudio.exe"
+new-alias np "C:\Program Files\Notepad++\notepad++.exe"
 # useful scripts
 new-alias clean c:\tools\clean.ps1
 new-alias find c:\tools\find.ps1
+new-alias lookup c:\tools\ip-lookup.ps1
 
 # starts a process in admin mode.
 new-alias admin c:\tools\start-as-admin.ps1
 
 # this sets up the password to be used in the admin alias when starting a process in admin mode.
 new-alias passwd c:\tools\set-password.ps1
+
+new-alias access c:\tools\check-access.ps1
+
+new-alias usr c:\tools\ad-search-user.ps1
+new-alias grp c:\tools\ad-search-group.ps1
